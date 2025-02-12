700fd3bab014058e43758c30cc3a088d3ca5a96d
Murzuqisah <amosjoel91@gmail.com>
2025-02-12 17:13:22 +0300

    Fix server listening method in the HTTP server implementation

--- js/server.js
+++ js/server.js
@@ -28,6 +28,6 @@ const httpServer = createServer(async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-Server.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
