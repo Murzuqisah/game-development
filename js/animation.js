31d44bd1555220245bce4433ba9fea265e117028
Murzuqisah <amosjoel229@gmail.com>
2025-02-23 00:15:39 +0300

    feat: Import additional functions for enhanced game mechanics

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,6 @@
 import { gameState, gameLoopID, accumulator, lastFPSTime, fps, frameCount } from "./init.js";
 import { updateGame } from "./move.js"
-import { draw, updateHUD } from "./main.js"
+import { draw, updateHUD, updateBalls, moveBat, checkLevelCompletion } from "./main.js"
 
 const TIME_STEP_MS = 1000 / 60;
 const TIME_STEP_S = 1 / 60;
@@ -8,6 +8,7 @@ const TIME_STEP_S = 1 / 60;
 let previousTime = performance.now();
 
 export const gameLoop = (currentTime) => {
+
     if (gameState.value !== 'running') return;
 
     let frameTime = currentTime - previousTime;
